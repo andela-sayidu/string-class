789b482d9313ea664498cac40d4031109f0eb076
andela-sayidu <shalom.ayidu@andela.com>
2016-12-11 17:56:21 +0100

    method(toUpper): converts string to uppercase

--- src/string.js
+++ src/string.js
@@ -7,7 +7,19 @@ const extendsStringClass = {
    */
   hasVowels() {
     return /[aeiou]/i.test(this);
+  },
+
+  /**
+   * toUpper()
+   * Returns string with all characters in upper case.
+   * @returns {String}
+   */
+  toUpper() {
+    return this.replace(/[a-z]/g, (match) => {
+      return String.fromCharCode(match.charCodeAt(0) - 32);
+    });
   }
+
 };
 
 Object.assign(String.prototype, extendsStringClass);
--- tests/string-test.js
+++ tests/string-test.js
@@ -11,3 +11,10 @@ describe('hasVowels', () => {
     expect(falseWord.hasVowels()).to.not.be.true;
   });
 });
+
+describe('toUpper', () => {
+  it('returns true if the string is returned in Uppercase', () => {
+    const word = 'uppercase';
+    expect(word.toUpperCase()).to.equal('UPPERCASE');
+  });
+});
